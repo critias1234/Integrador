88db67d00e0cd32561104f883a97e15a2943ba52
marcial <marcialgonzalezsk8@gmail.com>
2023-06-18 22:57:50 -0300

    modificación final

--- main.js
+++ main.js
@@ -1,15 +1,32 @@
 document.addEventListener('DOMContentLoaded', function() {
-    let btnResumen = document.querySelector('.btn-resumen');
-    let btnBorrar = document.querySelector('.btn-borrar');
+    let btnSummary = document.querySelector('.btn-summary');
+    let btnDelete = document.querySelector('.btn-delete');
 
-    btnResumen.addEventListener('click', function() {
-        let inputName = document.getElementById('inputName')
-        let inputSurname = document.getElementById('inputSurname')
-        let inputEmail = document.getElementById('inputEmail')
+    let studentButton = document.querySelector("#student");
+    let traineeButton = document.querySelector("#trainee");
+    let juniorButton = document.querySelector("#junior");
+    let inputState = document.querySelector("#inputState");
+
+    studentButton.addEventListener("click", function() {
+        inputState.value = "Estudiante";
+    });
+
+    traineeButton.addEventListener("click", function() {
+        inputState.value = "Trainee";
+    });
+
+    juniorButton.addEventListener("click", function() {
+        inputState.value = "Junior";
+    });
+    
+    btnSummary.addEventListener('click', function() {
+        let inputName = document.getElementById('inputName');
+        let inputSurname = document.getElementById('inputSurname');
+        let inputEmail = document.getElementById('inputEmail');
         let inputCant = document.getElementById('inputCant');
         let inputState = document.getElementById('inputState');
         let outputBox = document.querySelector('.output-box');
-        let totalPagar = inputCant.value * 200;
+        let totalPay = inputCant.value * 200;
 
         if (inputName.value === '' || inputSurname.value === '' || inputEmail.value === ''){
             alert('Por favor completa todos los campos del formulario');
@@ -17,18 +34,18 @@ document.addEventListener('DOMContentLoaded', function() {
         } 
 
         if (inputState.value === 'Estudiante') {
-            totalPagar = totalPagar * 0.8;
+            totalPay = totalPay * 0.8;
             } else if (inputState.value === 'Trainee'){
-                totalPagar = totalPagar * 0.5;
+                totalPay = totalPay * 0.5;
             } else if (inputState.value === 'Junior'){
-                totalPagar = totalPagar * 0.15;
+                totalPay = totalPay * 0.15;
             } 
 
-            outputBox.textContent = 'Total a Pagar: $' + totalPagar.toFixed(2);
+            outputBox.textContent = 'Total a Pagar: $' + totalPay.toFixed(2);
             alert('Formulario enviado con éxito!!');
     });
 
-    btnBorrar.addEventListener('click', function() {
+    btnDelete.addEventListener('click', function() {
         document.getElementById('inputName').value = '';
         document.getElementById('inputSurname').value = '';
         document.getElementById('inputEmail').value = '';
